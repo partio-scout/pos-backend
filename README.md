c51b3f120a2ca3c385edc48fd5e18e6ccb0aea99
Emmi Linkola <emmi.linkola@wunderdog.fi>
2019-11-20 14:32:31 +0200

    Initial scaffolding

---
+++ README.md
@@ -0,0 +1,1 @@
+# Partio-ohjelmasovellus frontend
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+
+app.use(cors())
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+// eslint-disable-next-line
+app.listen(process.env.PORT || 3001, () => console.log('Server listening!'))
