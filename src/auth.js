9f6366a14ec1097d038aef68fb28a6be01ba738b
Emmi Linkola <emmi.linkola@wunderdog.fi>
2019-12-20 09:51:54 +0200

    PartioID login WIP

---
+++ src/auth.js
@@ -0,0 +1,50 @@
+const passport = require('passport')
+const metadata = require('passport-saml-metadata')
+const SamlStrategy = require('passport-saml').Strategy
+
+const issuer = 'https://suunnittelu.beta.partio-ohjelma.fi'
+
+const metadataConfig = {
+  url: 'https://partioid-test.partio.fi/simplesaml/saml2/idp/metadata.php',
+  timeout: 30000,
+}
+
+module.exports.configurePassport = async () => {
+  try {
+    const reader = await metadata.fetch(metadataConfig)
+
+    const strategyConfig = metadata.toPassportConfig(reader)
+    Object.assign(strategyConfig, {
+      realm: issuer,
+      issuer,
+      protocol: 'samlp',
+      callbackUrl: 'http://localhost:4000/login/callback',
+    })
+
+    const samlStrategy = new SamlStrategy(strategyConfig, function(
+      profile,
+      done
+    ) {
+      console.log('profile:', profile)
+      return done(null, profile)
+    })
+
+    passport.use('saml', samlStrategy)
+
+    passport.serializeUser((user, done) => {
+      console.log('serializeUser:', user)
+      done(null, user)
+    })
+
+    passport.deserializeUser((user, done) => {
+      console.log('deserializeUser:', user)
+      done(null, user)
+    })
+
+    console.log(strategyConfig)
+
+    return samlStrategy
+  } catch (e) {
+    console.error('Failed to fetch SAML metadata', e)
+  }
+}
--- src/index.js
+++ src/index.js
@@ -2,37 +2,90 @@ require('dotenv').config()
 import express from 'express'
 import cors from 'cors'
 import bodyParser from 'body-parser'
+import passport from 'passport'
 import { postTaskEntry, getTaskEntries } from './database'
+import { configurePassport } from './auth'
+const cookieParser = require('cookie-parser')
+const cookieSession = require('cookie-session')
 
-const app = express()
+const main = async () => {
+  await configurePassport()
 
-app.use(cors())
-app.use(bodyParser.json())
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
+  const app = express()
+  app.use(cookieParser())
+
+  app.use(bodyParser.json())
+  app.use(
+    bodyParser.urlencoded({
+      extended: true,
+    })
+  )
+
+  app.use(
+    cookieSession({
+      name: 'session',
+      keys: ['key1'],
+
+      // Cookie Options
+      maxAge: 24 * 60 * 60 * 1000, // 24 hours
+    })
+  )
+
+  const corsOptions = {
+    origin: [
+      'http://localhost:3000',
+      'https://partioid-test.partio.fi',
+      'https://id.partio.fi',
+    ],
+    credentials: true,
+  }
+
+  app.use(cors(corsOptions))
+
+  app.use(passport.initialize())
+  app.use(passport.session())
+
+  app.get('/', (_, res) => res.send('OK'))
+
+  app.get(
+    '/login',
+    passport.authenticate('saml', {
+      successRedirect: '/success',
+      failureRedirect: '/failure',
+    })
+  )
+
+  app.post(
+    '/login/callback',
+    passport.authenticate('saml', {
+      failureRedirect: '/',
+      failureFlash: true,
+    }),
+    (req, res) => {
+      console.log('/login/callback')
+      res.redirect('http://localhost:3000/')
+    }
+  )
+
+  app.post('/task-entry', async (req, res) => {
+    try {
+      const id = await postTaskEntry(req.body)
+      res.json(id).status(200)
+    } catch (e) {
+      res.status(e.statusCode).send(e.message)
+    }
   })
-)
 
-app.post('/task-entry', async (req, res) => {
-  try {
-    const id = await postTaskEntry(req.body)
-    res.json(id).status(200)
-  } catch (e) {
-    res.status(e.statusCode).send(e.message)
-  }
-})
+  app.get('/task-entries/:userGuid', async (req, res) => {
+    try {
+      const entries = await getTaskEntries(req.params.userGuid)
+      res.json(entries).status(200)
+    } catch (e) {
+      res.status(e.statusCode).send(e.message)
+    }
+  })
 
-app.get('/task-entries/:userGuid', async (req, res) => {
-  try {
-    const entries = await getTaskEntries(req.params.userGuid)
-    res.json(entries).status(200)
-  } catch (e) {
-    res.status(e.statusCode).send(e.message)
-  }
-})
+  app.listen(3001, () => console.log('listening on port 3001'))
+}
 
-app.get('/', (req, res) => res.send('Hello World!'))
-
-// eslint-disable-next-line
-app.listen(process.env.PORT || 3001, () => console.log('Server listening!'))
+main().catch(error => console.error(error))
