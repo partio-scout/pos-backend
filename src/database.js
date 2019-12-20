7cd3a0b6b50889edef6e9431054018adc1155f48
Emmi Linkola <emmi.linkola@wunderdog.fi>
2019-12-20 16:36:31 +0200

    Add endpoint for posting task entries

--- src/database.js
+++ src/database.js
@@ -1,6 +1,19 @@
+require('dotenv').config()
 import pgPromise from 'pg-promise'
 
 const pgp = pgPromise()
 export const db = pgp(process.env.DATABASE_URL)
 
-console.log(db) // eslint-disable-line
+export async function postTaskEntry(taskEntry) {
+  const { user_guid, created_by, task_guid, completion_status } = taskEntry
+
+  try {
+    const data = await db.one(
+      'INSERT INTO task_entries(user_guid, created_by, task_guid, completion_status) VALUES ($1, $2, $3, $4) RETURNING id',
+      [user_guid, created_by, task_guid, completion_status]
+    )
+    console.log(data.id)
+  } catch (error) {
+    console.log('error', error)
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 require('dotenv').config()
 import express from 'express'
 import cors from 'cors'
+import bodyParser from 'body-parser'
+import { postTaskEntry } from './database'
 
 const app = express()
 
 app.use(cors())
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.post('/task-entry', async (req, res) => {
+  try {
+    await postTaskEntry(req.body)
+    res.sendStatus(200)
+  } catch (e) {
+    res.status(e.statusCode).send(e.message)
+  }
+})
 
 // eslint-disable-next-line
 app.listen(process.env.PORT || 3001, () => console.log('Server listening!'))
