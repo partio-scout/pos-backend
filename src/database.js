63b6337487171ea55975f4dac92251959783fbfa
Miika Kasnio <miika.kasnio@wunderdog.fi>
2020-01-31 17:35:32 +0200

    Return entry after saving

--- src/database.js
+++ src/database.js
@@ -12,7 +12,13 @@ export async function postTaskEntry(taskEntry) {
       'INSERT INTO task_entries(user_guid, created_by, task_guid, completion_status) VALUES ($1, $2, $3, $4) RETURNING id',
       [user_guid, created_by, task_guid, completion_status]
     )
-    return data.id
+
+    const entry = await db.one(
+      'SELECT task_guid, completion_status from task_entries WHERE id = $1',
+      data.id
+    )
+
+    return entry
   } catch (error) {
     console.log('error', error)
   }
