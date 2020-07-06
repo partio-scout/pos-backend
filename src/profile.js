22d264d98644901fb88412c07caf482750c2f487
Miikka Niemelä <miikka.niemela@orangit.fi>
2020-07-06 10:57:22 +0300

    Return only member info

--- src/profile.js
+++ src/profile.js
@@ -1,31 +1,8 @@
-import { getFavouriteTasks, getTaskEntries } from './database'
 import { getMember, getMemberImage } from './kuksa'
 
 export async function getProfile(userNumber) {
   const member = await getMember(userNumber)
   const memberImage = await getMemberImage(userNumber)
-  const favourites = await getFavouriteTasks(userNumber)
-  const favouriteIds = favourites.map(task => {
-    return task.task_guid
-  })
-  const tasks = await getTaskEntries(userNumber)
-  const ongoing = tasks
-    .filter(task => {
-      return (
-        task.completion_status === 'ACTIVE' ||
-        task.completion_status === 'COMPLETION_REQUESTED'
-      )
-    })
-    .map(ongoingTask => {
-      return ongoingTask.task_guid
-    })
-  const achieved = tasks
-    .filter(task => {
-      return task.completion_status === 'COMPLETED'
-    })
-    .map(achievedTask => {
-      return achievedTask.task_guid
-    })
 
   const profile = Object.assign(
     {},
@@ -38,11 +15,6 @@ export async function getProfile(userNumber) {
       troops: member.troops,
       isLeader: member.is_leader || false,
       image: memberImage,
-      favourites: favouriteIds,
-      ongoing: ongoing,
-      //TODO: somewhere else, when task is approved, it must be checked if that completes the whole taskgroup
-      //TODO: when a whole taskgroup gets achieved, it must be checked if the whole agegroup is completed
-      achieved: achieved,
     }
   )
 
