e7b980f2d4dce619e2802297a46a466593470988
Mika Kattainen <mika.kattainen@orangit.fi>
2020-09-24 13:38:56 +0300

    Fix error handling in groups.js

--- src/groups.js
+++ src/groups.js
@@ -43,39 +43,41 @@ async function getAllGroups(userNumber) {
   const member = await getMember(userNumber)
   const memberGroups = await getGroupsFromKuksa(userNumber)
   const groupAndMemberData = Promise.all(
-    memberGroups.groups
-      .map(async group => {
-        const groupInfo = await getGroupInfo(userNumber, group.id)
-        const groupMembers = await getGroupMembers(userNumber, group.id)
-        const memberData = await getMemberData(groupMembers)
-        const ageGroupId =
-          (memberData[0] &&
-            (await getMember(memberData[0].memberId)).age_groupId) ||
-          4
-        return Object.assign(
-          {},
-          {
-            id: group.id,
-            name: group.name,
-            ageGroup: groupInfo.age_groups[0],
-            ageGroupId,
-            troop: groupInfo.troops.filter(
-              troop => troop.id === member.default_troop_id
-            ),
-            members: memberData,
-          }
-        )
-      })
-      .catch(error => {
-        console.error('Failed to get all groups: ', error)
-      })
-  )
+    memberGroups.groups.map(async group => {
+      const groupInfo = await getGroupInfo(userNumber, group.id)
+      const groupMembers = await getGroupMembers(userNumber, group.id)
+      const memberData = await getMemberData(groupMembers)
+      const ageGroupId =
+        (memberData[0] &&
+          (await getMember(memberData[0].memberId)).age_groupId) ||
+        4
+      return Object.assign(
+        {},
+        {
+          id: group.id,
+          name: group.name,
+          ageGroup: groupInfo.age_groups[0],
+          ageGroupId,
+          troop: groupInfo.troops.filter(
+            troop => troop.id === member.default_troop_id
+          ),
+          members: memberData,
+        }
+      )
+    })
+  ).catch(error => {
+    console.error('Failed to get groupAndMemberData: ', error)
+  })
 
   return groupAndMemberData
 }
 
 export async function getGroups(userNumber) {
-  const allGroups = await getAllGroups(userNumber)
-  const filteredGroups = filterGroups(userNumber, allGroups)
-  return filteredGroups || []
+  try {
+    const allGroups = await getAllGroups(userNumber)
+    const filteredGroups = filterGroups(userNumber, allGroups)
+    return filteredGroups || []
+  } catch (error) {
+    console.error('Get groups failed with error: ', error)
+  }
 }
