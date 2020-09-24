525198e556935eaad166d6aedd45fc6dbc794f82
Mika Kattainen <mika.kattainen@orangit.fi>
2020-09-24 12:54:34 +0300

    Improve error catching in groups.js

--- src/groups.js
+++ src/groups.js
@@ -25,7 +25,9 @@ async function getMemberData(groupMembers) {
         }
       )
     })
-  )
+  ).catch(error => {
+    console.error('Failed to get member data: ', error)
+  })
 }
 
 function filterGroups(userNumber, groupsData) {
@@ -41,40 +43,39 @@ async function getAllGroups(userNumber) {
   const member = await getMember(userNumber)
   const memberGroups = await getGroupsFromKuksa(userNumber)
   const groupAndMemberData = Promise.all(
-    memberGroups.groups.map(async group => {
-      const groupInfo = await getGroupInfo(userNumber, group.id)
-      const groupMembers = await getGroupMembers(userNumber, group.id)
-      const memberData = await getMemberData(groupMembers)
-      const ageGroupId =
-        (memberData[0] &&
-          (await getMember(memberData[0].memberId)).age_groupId) ||
-        4
-      return Object.assign(
-        {},
-        {
-          id: group.id,
-          name: group.name,
-          ageGroup: groupInfo.age_groups[0],
-          ageGroupId,
-          troop: groupInfo.troops.filter(
-            troop => troop.id === member.default_troop_id
-          ),
-          members: memberData,
-        }
-      )
-    })
+    memberGroups.groups
+      .map(async group => {
+        const groupInfo = await getGroupInfo(userNumber, group.id)
+        const groupMembers = await getGroupMembers(userNumber, group.id)
+        const memberData = await getMemberData(groupMembers)
+        const ageGroupId =
+          (memberData[0] &&
+            (await getMember(memberData[0].memberId)).age_groupId) ||
+          4
+        return Object.assign(
+          {},
+          {
+            id: group.id,
+            name: group.name,
+            ageGroup: groupInfo.age_groups[0],
+            ageGroupId,
+            troop: groupInfo.troops.filter(
+              troop => troop.id === member.default_troop_id
+            ),
+            members: memberData,
+          }
+        )
+      })
+      .catch(error => {
+        console.error('Failed to get all groups: ', error)
+      })
   )
 
   return groupAndMemberData
 }
 
 export async function getGroups(userNumber) {
-  try {
-    const allGroups = await getAllGroups(userNumber)
-    const filteredGroups = filterGroups(userNumber, allGroups)
-    return filteredGroups
-  } catch (error) {
-    console.error('Failed to fetch groups: ', error)
-  }
-  return []
+  const allGroups = await getAllGroups(userNumber)
+  const filteredGroups = filterGroups(userNumber, allGroups)
+  return filteredGroups || []
 }
