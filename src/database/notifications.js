437983f175224d3b54bce7ed2d711a99b27872fc
Mika Kattainen <mika.kattainen@orangit.fi>
2021-07-07 15:54:08 +0300

    Add database queries for notifications

---
+++ src/database/notifications.js
@@ -0,0 +1,85 @@
+import { db } from './index'
+
+const TABLE_NAME = 'notifications'
+
+export async function createNotification(notification) {
+  const {
+    itemGuid,
+    itemType,
+    notificationType,
+    userGuid,
+    createdBy,
+  } = notification
+  try {
+    const data = await db.one(
+      `INSERT INTO ${TABLE_NAME} (item_guid, item_type, notification_type, user_guid, created_by) VALUES ($1, $2, $3, $4, $5) RETURNING id`,
+      [itemGuid, itemType, notificationType, userGuid, createdBy]
+    )
+
+    const notification = await db.one(
+      `SELECT * FROM ${TABLE_NAME} WHERE id = $1`,
+      data.id
+    )
+
+    return notification
+  } catch (error) {
+    console.log('error', error)
+  }
+}
+
+export async function markNotificationAsViewed(notificationId) {
+  try {
+    const data = await db.one(
+      `UPDATE ${TABLE_NAME} SET viewed=$1 WHERE id=$2 RETURNING viewed`,
+      [true, notificationId]
+    )
+    return data
+  } catch (error) {
+    console.log('error', error)
+  }
+}
+
+export async function markNotificationsAsViewed(userGuid) {
+  try {
+    const data = await db.any(
+      `UPDATE ${TABLE_NAME} SET viewed=$1 WHERE user_guid=$2 AND viewed!=$1`,
+      [true, userGuid]
+    )
+    return data
+  } catch (error) {
+    console.log('error', error)
+  }
+}
+
+export async function getNotification(notificationId) {
+  try {
+    const notification = await db.one(
+      `SELECT * FROM ${TABLE_NAME} WHERE id = $1`,
+      [notificationId]
+    )
+
+    return notification
+  } catch (error) {
+    console.log('error', error)
+  }
+}
+
+export async function getUserNotifications(user_guid) {
+  try {
+    const notifications = await db.any(
+      `SELECT * FROM ${TABLE_NAME} WHERE user_guid = $1 AND viewed = $2 ORDER BY created_at DESC`,
+      [user_guid, false]
+    )
+
+    if (!notifications || !notifications.length) {
+      return db.any(
+        `SELECT * FROM ${TABLE_NAME} WHERE user_guid = $1 ORDER BY created_at DESC LIMIT 5`,
+        user_guid.toString()
+      )
+    }
+
+    return notifications
+  } catch (error) {
+    console.log('error', error)
+  }
+}
