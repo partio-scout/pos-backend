12ba71d84e9957a400b17ba1bc41706bea2e15e2
Kaisa Korpela <kaisa.korpela@orangit.fi>
2021-08-10 16:28:33 +0300

    Edit schedule for testing  delete old notifications -job

--- src/index.js
+++ src/index.js
@@ -76,7 +76,7 @@ const main = async () => {
   const cron = require('node-cron')
 
   cron.schedule(
-    '15 16 * * *',
+    '35 16 * * *',
     () => {
       console.log('Delete old notifications')
       deleteOldNotifications()
