accd0ae870ac674f8d987de71fa6e56d2c78b098
Paula Rinta-Harri <paula.rinta-harri@orangit.fi>
2024-04-09 14:23:23 +0300

    Add names for new indexes

--- migrations/1712659673740_add-user-guid-indexes.js
+++ migrations/1712659673740_add-user-guid-indexes.js
@@ -6,11 +6,24 @@ exports.up = (pgm) => {
   // Adding indexes
   pgm.createIndex('completed_agegroup_entries', 'user_guid', {
     ifNotExists: true,
+    name: completed_agegroup_entries_user_guid_idx,
   })
-  pgm.createIndex('favourite_tasks', 'user_guid', { ifNotExists: true })
-  pgm.createIndex('notifications', 'user_guid', { ifNotExists: true })
-  pgm.createIndex('task_entries', 'user_guid', { ifNotExists: true })
-  pgm.createIndex('task_group_entries', 'user_guid', { ifNotExists: true })
+  pgm.createIndex('favourite_tasks', 'user_guid', {
+    ifNotExists: true,
+    name: favourite_tasks_user_guid_idx,
+  })
+  pgm.createIndex('notifications', 'user_guid', {
+    ifNotExists: true,
+    name: notifications_user_guid_idx,
+  })
+  pgm.createIndex('task_entries', 'user_guid', {
+    ifNotExists: true,
+    name: task_entries_user_guid_idx,
+  })
+  pgm.createIndex('task_group_entries', 'user_guid', {
+    ifNotExists: true,
+    name: task_group_entries_user_guid_idx,
+  })
 }
 
 exports.down = (pgm) => {}
