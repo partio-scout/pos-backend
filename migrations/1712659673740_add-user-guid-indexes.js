fb86fe705fbc8f27bac60fe39cc693e13f792ac2
Paula Rinta-Harri <paula.rinta-harri@orangit.fi>
2024-04-09 14:37:48 +0300

    Add migration script for adding user_guid indexes (#79)
    * Add migration script for adding user_guid indexes
    
    * Add names for new indexes

---
+++ migrations/1712659673740_add-user-guid-indexes.js
@@ -0,0 +1,29 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined
+
+exports.up = (pgm) => {
+  // Adding indexes
+  pgm.createIndex('completed_agegroup_entries', 'user_guid', {
+    ifNotExists: true,
+    name: completed_agegroup_entries_user_guid_idx,
+  })
+  pgm.createIndex('favourite_tasks', 'user_guid', {
+    ifNotExists: true,
+    name: favourite_tasks_user_guid_idx,
+  })
+  pgm.createIndex('notifications', 'user_guid', {
+    ifNotExists: true,
+    name: notifications_user_guid_idx,
+  })
+  pgm.createIndex('task_entries', 'user_guid', {
+    ifNotExists: true,
+    name: task_entries_user_guid_idx,
+  })
+  pgm.createIndex('task_group_entries', 'user_guid', {
+    ifNotExists: true,
+    name: task_group_entries_user_guid_idx,
+  })
+}
+
+exports.down = (pgm) => {}
