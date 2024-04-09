ac33ab0c896ba4cc973b3ff18598fc2d17b1c48c
Paula Rinta-Harri <paula.rinta-harri@orangit.fi>
2024-04-09 14:44:46 +0300

    Fix db index naming to migration script

--- migrations/1712659673740_add-user-guid-indexes.js
+++ migrations/1712659673740_add-user-guid-indexes.js
@@ -6,23 +6,23 @@ exports.up = (pgm) => {
   // Adding indexes
   pgm.createIndex('completed_agegroup_entries', 'user_guid', {
     ifNotExists: true,
-    name: completed_agegroup_entries_user_guid_idx,
+    name: 'completed_agegroup_entries_user_guid_idx',
   })
   pgm.createIndex('favourite_tasks', 'user_guid', {
     ifNotExists: true,
-    name: favourite_tasks_user_guid_idx,
+    name: 'favourite_tasks_user_guid_idx',
   })
   pgm.createIndex('notifications', 'user_guid', {
     ifNotExists: true,
-    name: notifications_user_guid_idx,
+    name: 'notifications_user_guid_idx',
   })
   pgm.createIndex('task_entries', 'user_guid', {
     ifNotExists: true,
-    name: task_entries_user_guid_idx,
+    name: 'task_entries_user_guid_idx',
   })
   pgm.createIndex('task_group_entries', 'user_guid', {
     ifNotExists: true,
-    name: task_group_entries_user_guid_idx,
+    name: 'task_group_entries_user_guid_idx',
   })
 }
 
